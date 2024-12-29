ef0fdef45c4e08eef60bf02f334022a84525a292
Anggreany <anggreanydwiandiny@gmail.com>
2024-12-29 11:50:51 +0800

    Add files via upload

---
+++ Latihan2_conditional.java
@@ -0,0 +1,17 @@
+package Latihan3_221141;
+
+public class Latihan2_conditional {
+   public static void main (String[] args){
+       int x = 0;
+       boolean isEven = false;
+       System.out.println("x="+x);
+       x =isEven ?4:7;
+       System.out.println("x="+x);
+       isEven = true;
+       x = isEven ? 4:7;
+       System.out.println("x=x"+x);
+                 
+             
+   }
+ 
+}
---
+++ TestArimatika.java
@@ -0,0 +1,20 @@
+package Latihan3_221141;
+
+public class TestArimatika {
+   public static void main (String[] args){
+       short x =6;
+       int y =4;
+       float a =12.5f;
+       float b = 7f;
+       System.out.println("x ="+x+",y="+y);
+       System.out.println("x+y="+(x+y));
+       System.out.println("x-y="+(x-y));
+       System.out.println("x*y="+(x*y));
+       System.out.println("x/y="+(x/y));
+       System.out.println("x%y="+(x%y));
+       System.out.println("a="+a+",b="+b);
+       System.out.println("a/b="+(a/b));
+       
+               
+   } 
+}
